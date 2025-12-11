e5b6c8ecc030383b444dc6b703d65f23a8585715
Sapanaad <sapnaad25@gmail.com>
2025-12-11 18:36:26 +0530

    First Commit

---
+++ src/test/java/com/tka/jbkcrm/SeleniumScript/Day1.java
@@ -0,0 +1,11 @@
+package com.tka.jbkcrm.SeleniumScript;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.instagram.com/accounts/login/?hl=en");
+		}
+}

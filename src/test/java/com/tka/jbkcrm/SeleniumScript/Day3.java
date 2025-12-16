561506933116b3f24ff512f513cef01e73b46154
Sapanaad <sapnaad25@gmail.com>
2025-12-16 12:52:26 +0530

    Added Maven Selenium project

--- src/test/java/com/tka/jbkcrm/SeleniumScript/Day3.java
+++ src/test/java/com/tka/jbkcrm/SeleniumScript/Day3.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Day1 {
+public class Day3 {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
@@ -26,3 +26,4 @@ public class Day1 {
 			System.out.println("Test case Failed");
 		}
 }
+
